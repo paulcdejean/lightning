40ed6c521abb965e992a0163294d90047757d994
Paul Dejean <paulcdejean@gmail.com>
2026-01-02 17:26:11 -0600

    Working!!!

---
+++ 06_eks_cluster/sso_admin_access.tf
@@ -0,0 +1,30 @@
+# Important note!
+# It's important to attach cluster admin directly to the user.
+# If you instead attach it to a role and have the user assume that role, then they won't have access in the console.
+# Yes even if they're a AWS admin.
+
+data "aws_region" "current" {}
+
+data "aws_iam_roles" "admin" {
+  path_prefix = "/aws-reserved/sso.amazonaws.com/${data.aws_region.current.region}/"
+  name_regex  = "AWSReservedSSO_admin_.*"
+}
+
+data "aws_iam_role" "admin" {
+  name = one(data.aws_iam_roles.admin.names)
+}
+
+resource "aws_eks_access_entry" "admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = data.aws_iam_role.admin.arn
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
+  principal_arn = data.aws_iam_role.admin.arn
+  access_scope {
+    type = "cluster"
+  }
+}
