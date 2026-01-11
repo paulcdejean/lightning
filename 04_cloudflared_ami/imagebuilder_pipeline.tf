4fcbe89ebf9c19f48fa8f6ba235e1deb3e81eac3
Paul Dejean <paulcdejean@gmail.com>
2026-01-11 17:30:48 -0600

    Upgraded AWS provider and used the new feature, imagebuilder log groups!

--- 04_cloudflared_ami/imagebuilder_pipeline.tf
+++ 04_cloudflared_ami/imagebuilder_pipeline.tf
@@ -4,6 +4,10 @@ resource "aws_imagebuilder_image_pipeline" "cloudflared" {
   infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.lightning.arn
   distribution_configuration_arn   = aws_imagebuilder_distribution_configuration.cloudflared.arn
   execution_role                   = aws_iam_role.imagebuilder_execution.arn
+  logging_configuration {
+    image_log_group_name    = aws_cloudwatch_log_group.image_log_group.name
+    pipeline_log_group_name = aws_cloudwatch_log_group.pipeline_log_group.name
+  }
   lifecycle {
     replace_triggered_by = [
       aws_imagebuilder_image_recipe.cloudflared
