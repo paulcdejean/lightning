3a5b9a7fa51c6fd11af60fd64663568765c3fbfe
Paul Dejean <paulcdejean@gmail.com>
2026-01-16 17:39:58 -0600

    So much weird stuff...

--- 09_aws_cni/helm.tf
+++ 09_aws_cni/helm.tf
@@ -3,19 +3,31 @@ resource "helm_release" "aws_cni" {
   namespace  = "kube-system"
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-vpc-cni"
-  version    = "1.21.1"
+  version    = local.workspace.version
   set = [
     {
       name  = "image.overrideRepository"
       value = "ecr-public.aws.com/eks/amazon-k8s-cni-init"
     },
     {
+      name  = "image.tag"
+      value = "v${local.workspace.version}"
+    },
+    {
       name  = "init.image.overrideRepository"
       value = "ecr-public.aws.com/eks/amazon-k8s-cni-init"
     },
     {
+      name  = "init.image.tag"
+      value = "v${local.workspace.version}"
+    },
+    {
       name  = "nodeAgent.image.overrideRepository"
       value = "ecr-public.aws.com/eks/amazon-k8s-cni-init"
+    },
+    {
+      name  = "nodeAgent.image.tag"
+      value = "v${local.workspace.version}"
     },
   ]
 }
--- 09_aws_cni/workspace.tf
+++ 09_aws_cni/workspace.tf
@@ -2,6 +2,7 @@ locals {
   workspace = local.workspaces[tofu.workspace]
   workspaces = {
     unstable = {
+      version = "1.21.1"
     }
   }
 }
