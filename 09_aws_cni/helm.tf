cdcdb6d3ae4b39faa80bb28b525ea5dfc2c32b4a
Paul Dejean <paulcdejean@gmail.com>
2026-01-16 17:55:59 -0600

    More ipv6 fun.

--- 09_aws_cni/helm.tf
+++ 09_aws_cni/helm.tf
@@ -5,9 +5,10 @@ resource "helm_release" "aws_cni" {
   chart      = "aws-vpc-cni"
   version    = local.workspace.version
   set = [
+    # Image overrides for dualstack endpoints.
     {
       name  = "image.overrideRepository"
-      value = "ecr-public.aws.com/eks/amazon-k8s-cni-init"
+      value = "ecr-public.aws.com/eks/amazon-k8s-cni"
     },
     {
       name  = "image.tag"
@@ -23,11 +24,28 @@ resource "helm_release" "aws_cni" {
     },
     {
       name  = "nodeAgent.image.overrideRepository"
-      value = "ecr-public.aws.com/eks/amazon-k8s-cni-init"
+      value = "ecr-public.aws.com/eks/aws-network-policy-agent"
     },
     {
       name  = "nodeAgent.image.tag"
       value = "v${local.workspace.version}"
+    },
+    # Disable ipv4, enable ipv6.
+    {
+      name  = "init.env.ENABLE_IPv6"
+      value = "true"
+    },
+    {
+      name  = "nodeAgent.enableIpv6"
+      value = "true"
+    },
+    {
+      name  = "env.ENABLE_IPv6"
+      value = "true"
+    },
+    {
+      name  = "env.ENABLE_IPv4"
+      value = "false"
     },
   ]
 }
