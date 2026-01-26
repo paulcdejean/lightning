1733f7ace00e2833fb232ce6d5f3bb97c6660936
Paul Dejean <paulcdejean@gmail.com>
2026-01-25 19:22:55 -0600

    The thing.

--- 10_kubenode_ami/templates/kubelet_wrapper.bash
+++ 10_kubenode_ami/templates/kubelet_wrapper.bash
@@ -22,6 +22,8 @@ private_hostname=$(ec2-metadata --local-hostname --quiet)
 ipv4_info=$(ip -json -br -4 addr show scope global)
 ipv4_addr=$(jq -r '.[0]["addr_info"][0]["local"]' <<< "$ipv4_info")
 ipv4_length=$(jq -r '.[0]["addr_info"][0]["prefixlen"]' <<< "$ipv4_info")
+ipv6_info=$(ip -json -br -6 addr show scope global)
+ipv6_addr=$(jq -r '.[0]["addr_info"][0]["local"]' <<< "$ipv6_info")
 adder_number="$(( 2 ** (31 - $ipv4_length) ))"
 eni_addr=$(python3 -c "import ipaddress; print(ipaddress.IPv4Address('$ipv4_addr') + $adder_number)")
 eni_info=$(aws --output json ec2 describe-network-interfaces --filters "Name=private-ip-address,Values=$eni_addr")
@@ -29,6 +31,8 @@ ipv6_prefix=$(jq -r '.NetworkInterfaces[0].Ipv6Prefixes[0].Ipv6Prefix' <<< "$eni
 eni_id=$(jq -r '.NetworkInterfaces[0].NetworkInterfaceId' <<< "$eni_info")
 eni_mac=$(jq -r '.NetworkInterfaces[0].MacAddress' <<< "$eni_info")
 instance_id=$(ec2-metadata --instance-id --quiet)
+cluster_name=$(cat /etc/lightning/cluster_name.txt)
+cluster_endpoint=$(aws eks describe-cluster --name lightning-unstable --query "cluster.endpoint" --output text)
 
 cat | tee /etc/systemd/network/10-kube.link << HEREDOC
 [Match]
@@ -46,9 +50,6 @@ udevadm wait /sys/class/net/kube
 ip addr add "$ipv6_prefix" dev kube
 ip a
 
-cluster_name=$(cat /etc/lightning/cluster_name.txt)
-cluster_endpoint=$(aws eks describe-cluster --name lightning-unstable --query "cluster.endpoint" --output text)
-
 # Put the ca data to a file
 aws eks describe-cluster --name lightning-unstable --query "cluster.certificateAuthority.data" --output text | base64 -d > /etc/lightning/pki_ca.crt
 
@@ -64,6 +65,15 @@ sed -i \
   -e s%__ZONE__%$availability_zone% \
   -e s%__INSTNACE__%$instance_id% \
   /etc/lightning/kublet_config.yaml
+
+# Configure cilium node
+sed -i \
+  -e s%__HOSTNAME__%$private_hostname% \
+  -e s%__IPV4__%$ipv4_addr% \
+  -e s%__IPV6__%$ipv6_addr% \
+  -e s%__INSTANCE__%$instance_id% \
+  -e s%__PREFIX__%$ipv6_prefix% \
+  /etc/lightning/cilium_node.yaml
 
 # Notes:
 # Cluster name format according to the AWS console:
