7d43ebaa4bfa868990f7b85c8896ee8e6b48c998
Paul Dejean <paulcdejean@gmail.com>
2026-02-18 18:00:34 -0600

    Networking tweaks.

--- 02_vpc/workspace.tf
+++ 02_vpc/workspace.tf
@@ -5,7 +5,7 @@ locals {
       ipv4_cidr_prefix = "10.10."
       ipv4_cidr_suffix = "0.0/16"
       # Our /52 can have 16 environments inside it.
-      ipv6_hexnumber = "1" # Skipping 0 because it makes things visually easier for me.
+      ipv6_hexnumber = "5"
     }
   }
 }
