eb6e7600ae2691ad6e7b569bbd3412e21e48bdb5
Paul Dejean <paulcdejean@gmail.com>
2026-02-18 18:11:40 -0600

    Add pods only subnet to this layer, no need for a new layer.

--- 07_kubenode_networking/workspace.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  workspace = local.workspaces[tofu.workspace]
-  workspaces = {
-    unstable = {
-      # Enable connecting directly to nodes and pods through the tunnel, for troubleshooting.
-      enable_kubenode_debugging = true
-    }
-  }
-}
