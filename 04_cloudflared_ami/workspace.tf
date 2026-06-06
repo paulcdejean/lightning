ee778935f240d38922344b9873da90eb7c3d6b28
Paul Dejean <paulcdejean@gmail.com>
2026-06-06 18:45:24 -0500

    Uhh ok.

--- 04_cloudflared_ami/workspace.tf
+++ 04_cloudflared_ami/workspace.tf
@@ -2,9 +2,9 @@ locals {
   workspace = local.workspaces[tofu.workspace]
   workspaces = {
     unstable = {
-      region = "us-east-2"
-      az           = "us-east-2a"
-      logs_bucket  = "pauldejean-logs"
+      region      = "us-east-2"
+      az          = "us-east-2a"
+      logs_bucket = "pauldejean-logs"
     }
   }
 }
