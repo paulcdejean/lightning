126dbcb6f0f67a81aa540567e90da154494fb01f
Paul Dejean <paulcdejean@gmail.com>
2026-06-06 17:26:42 -0500

    What happened?

---
+++ 04_cloudflared_ami/log_bucket.tf
@@ -0,0 +1,7 @@
+# data "aws_caller_identity" "current" {}
+# data "aws_region" "current" {}
+
+resource "aws_s3_bucket" "imagebuilder_logs" {
+  bucket           = format("lightning-imagebuilder-logs-%s-%s-an", data.aws_caller_identity.current.account_id, data.aws_region.current.name)
+  bucket_namespace = "account-regional"
+}
