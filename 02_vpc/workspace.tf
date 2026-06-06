87d8509ab5e18bcbc6173805428125e976405e94
Paul Dejean <paulcdejean@gmail.com>
2026-06-06 18:29:00 -0500

    fmt

--- 02_vpc/workspace.tf
+++ 02_vpc/workspace.tf
@@ -2,7 +2,7 @@ locals {
   workspace = local.workspaces[tofu.workspace]
   workspaces = {
     unstable = {
-      region = "us-east-2"
+      region           = "us-east-2"
       ipv4_cidr_prefix = "10.10."
       ipv4_cidr_suffix = "0.0/16"
       # Our /52 can have 16 environments inside it.
--- 03_bastion/workspace.tf
+++ 03_bastion/workspace.tf
@@ -2,7 +2,7 @@ locals {
   workspace = local.workspaces[tofu.workspace]
   workspaces = {
     unstable = {
-      region = "us-east-2"
+      region                    = "us-east-2"
       state_bucket              = "lightning-593941967609-us-east-2-an"
       instance_type             = "t4g.medium"
       enabled                   = false
