e34b1af389517e8da014f5b5f45679341552f9ab
Paul Dejean <paulcdejean@gmail.com>
2026-06-19 13:09:03 -0500

    Chores.

--- 02_vpc/tofu.tf
+++ 02_vpc/tofu.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "1.12.2"
+  required_version = "1.12.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.50.0"
+      version = "6.51.0"
     }
     toml = {
       source  = "Tobotimus/toml"
