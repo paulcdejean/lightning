25b7b9082915874778757fe406c5eb7ecd90e578
Paul Dejean <paulcdejean@gmail.com>
2026-06-19 19:45:14 -0500

    AI has a long way to go.

--- 00_aws_users/cloudflare.tf
+++ 00_aws_users/cloudflare.tf
@@ -12,6 +12,21 @@ locals {
     group.name => group.id
     if contains(group.scopes, "com.cloudflare.api.account")
   }
+  cf_perms_scope_to_ids = transpose({
+    for group in data.cloudflare_account_api_token_permission_groups_list.all.result :
+    group.id => group.scopes
+  })
+  cf_perms_ids_to_names = {
+    for group in data.cloudflare_account_api_token_permission_groups_list.all.result :
+    group.id => group.name
+  }
+  cf_perm_groups = {
+    for k, v in local.cf_perms_scope_to_ids :
+    k => {
+      for id in v :
+      local.cf_perms_ids_to_names[id] => id
+    }
+  }
 }
 
 resource "cloudflare_ai_gateway" "lightning" {
@@ -26,6 +41,10 @@ resource "cloudflare_ai_gateway" "lightning" {
   retry_backoff              = "exponential"
   retry_delay                = 500
   retry_max_attempts         = 3
+  logpush                    = false
+  log_management_strategy    = "DELETE_OLDEST"
+  log_management             = 100000
+  zdr                        = false
 }
 
 resource "cloudflare_account_token" "agent" {
@@ -38,8 +57,8 @@ resource "cloudflare_account_token" "agent" {
       "com.cloudflare.api.account.${local.cf_account_id}" = "*"
     })
     permission_groups = [
-      { id = local.cf_permission_groups["Workers AI Read"] },
-      { id = local.cf_permission_groups["AI Gateway Read"] },
+      { id = local.cf_perm_groups["com.cloudflare.api.account"]["Workers AI Read"] },
+      { id = local.cf_perm_groups["com.cloudflare.api.account"]["AI Gateway Read"] },
     ]
   }]
 }
