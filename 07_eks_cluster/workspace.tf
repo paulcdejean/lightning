020877fab76f5454c744bc5765c1aabdd05afceb
Paul Dejean <paulcdejean@gmail.com>
2026-06-20 22:02:14 -0500

    Renaming.

--- 07_eks_cluster/workspace.tf
+++ 07_eks_cluster/workspace.tf
@@ -2,7 +2,7 @@ locals {
   workspace = local.workspaces[tofu.workspace]
   workspaces = {
     unstable = {
-      kube_version              = "1.34"
+      kube_version              = "1.36"
       region                    = "us-east-2"
       enable_kubenode_debugging = true
     }
