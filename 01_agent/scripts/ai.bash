9103f5d352479cc3e94d06993c01b85774026809
Paul Dejean <paulcdejean@gmail.com>
2026-06-27 16:52:14 -0500

    Some todos for the jailed agent launching script

--- 01_agent/scripts/ai.bash
+++ 01_agent/scripts/ai.bash
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -euo pipefail
-# Launches the AI container jail
 
-# TODO, start container system if it's stopped
+if ! container system status > /dev/null ; then
+  container system start
+fi
 
-# TODO, builder container might need to be relaunched to correct dns settings
-
-# TODO, determine this automatically from container system settings
 localhost_ip="203.0.113.113"
-
 git_dir=$(git rev-parse --show-toplevel)
+tag=lightning-agent:$(date +%Y%m%d)
 
-# TODO, rebuild from scratch if over 24 hours old, otherwise skip build
-image_id=$(container build --dns $localhost_ip -f $git_dir/01_agent/jail.containerfile $git_dir/01_agent)
+if ! container image inspect lightning-agent:$tag > /dev/null ; then
+  container build --pull --no-cache -t $tag --dns $localhost_ip -f $git_dir/01_agent/jail.containerfile $git_dir/01_agent
+fi
 
-exec container run --rm --dns $localhost_ip --env-file $git_dir/01_agent/.env -v $git_dir:/root/lightning/ -it $image_id
+exec container run --rm --dns $localhost_ip --env-file $git_dir/01_agent/.env -v $git_dir:/root/lightning/ -it $tag
